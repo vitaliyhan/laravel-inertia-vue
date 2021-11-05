b16682e16189517b9dcb65ad6f302fda2548e9dd
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-11-05 19:46:15 +0300

    created inertia app and all works

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Inertia\Inertia;
+
+class IndexController extends Controller
+
+
+{
+    public function show()
+    {
+        return Inertia::render('Home', [
+            'title' => 'Home inertia'
+        ]);
+    }
+}
+
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Home {{ title }}</h1>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String
+    }
+
+}
+</script>
+
+
